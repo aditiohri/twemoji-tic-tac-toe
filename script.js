4827f5ca7215fef22b4babcdb2df8f54104e96a8
Aditi Ohri <didiohri@gmail.com>
2021-06-10 20:23:59 -0500

    user can now pick emoji as an avatar

--- script.js
+++ script.js
@@ -2,25 +2,14 @@ import { EmojiButton } from "https://cdn.jsdelivr.net/npm/@joeattardi/emoji-butt
 
 const picker = new EmojiButton({ autoHide: false });
 const trigger = document.querySelector("#emoji-trigger");
-
+const gameboard = document.querySelector(".gameboard");
+let currentPlayer = "player1";
+let player = document.querySelector(`#${currentPlayer}`);
 
 picker.on("emoji", (selection) => {
-  trigger.textContent = selection.emoji;
+  player.textContent = selection.emoji;
+  currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
+  player = document.querySelector(`#${currentPlayer}`);
 });
 
 trigger.addEventListener("click", () => picker.togglePicker(trigger));
-// document.addEventListener(
-//   "DOMContentLoaded",
-
-// );
-// import { EmojiButton } from "@joeattardi/emoji-button";
-
-// const picker = new EmojiButton();
-// const trigger = document.querySelector("#emoji-trigger");
-
-// picker.on("emoji", (selection) => {
-//   // handle the selected emoji here
-//   console.log(selection.emoji);
-// });
-
-// trigger.addEventListener("click", () => picker.togglePicker(trigger));
